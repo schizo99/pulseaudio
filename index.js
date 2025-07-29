6d8283ded7315f9458d77941f3baf5e5716d5776
Andreas Åman <andreas.aman@atg.se>
2025-07-29 11:56:35 +0200

    use docker compose command

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const stopContainer = async () => {
 
 const startContainer = async () => {
   return new Promise((resolve, reject) => {
-    exec(`docker compose -f /app/lms/docker-compose.yml up -d`, (error, stdout, stderr) => {
+    exec(`docker compose -f /app/lms/docker-compose.yml up -d --no-recreate`, (error, stdout, stderr) => {
       if (error) {
           console.error(`error: ${error.message}`);
           reject(error);
